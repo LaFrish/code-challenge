bcc9363fc22ebba6475531597ea3165380530893
YOUR FULL NAME <farishta.haider@gmail.com>
2016-09-06 17:40:52 -0400

    added more cross browswer webkits

---
+++ banner2.js
@@ -0,0 +1,5 @@
+var clickTag = "https://www.geico.com/";
+ document.getElementById('wrapper').addEventListener('click', clickHandler, false);
+    function clickHandler(event) {
+        window.open(window.clickTag);
+    }
